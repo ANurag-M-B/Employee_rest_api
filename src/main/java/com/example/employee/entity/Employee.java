79fec1f69455e02a7713b1657f811e635b4429ae
Anurag <>
2025-04-23 12:38:23 +0530

    first commit, added entity and dao interface

---
+++ src/main/java/com/example/employee/entity/Employee.java
@@ -0,0 +1,67 @@
+package com.example.employee.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="employee")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="second_name")
+	private String secondName;
+	
+	@Column(name="email")
+	private String email;
+
+	public Employee() {
+	}
+	
+	
+
+	public Employee(String firstName, String secondName, String email) {
+		this.firstName = firstName;
+		this.secondName = secondName;
+		this.email = email;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getSecondName() {
+		return secondName;
+	}
+
+	public void setSecondName(String secondName) {
+		this.secondName = secondName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
